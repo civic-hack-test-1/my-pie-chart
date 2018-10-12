98f9394713a06511226a076592691f99d768570a
Andy Mayer <andy@yoomee.com>
2018-10-12 11:22:47 +0100

    Updates

--- hello.py
+++ hello.py
@@ -1,6 +1,29 @@
 from flask import Flask
-app = Flask(__name__)
+from xml.dom import minidom
 
+app = Flask(__name__)
 @app.route("/")
+
 def hello():
-    return "Hello World!"
+    html = "<h1>Restaurants in Durham</h1>"
+
+    xmldoc = minidom.parse("open-data.xml")
+    list = xmldoc.getElementsByTagName("EstablishmentDetail")
+    for n in list:
+        business = n.getElementsByTagName("BusinessName")[0].firstChild.data
+        rating = n.getElementsByTagName("RatingValue")[0].firstChild.data
+        type = n.getElementsByTagName("BusinessType")[0].firstChild.data
+
+        if rating == "AwaitingInspection":
+            rating = "pending"
+
+        if n.getElementsByTagName("RatingDate")[0].firstChild:
+            date = n.getElementsByTagName("RatingDate")[0].firstChild.data
+        else:
+            date = "none"
+
+        if type == "Restaurant/Cafe/Canteen":
+            html += "<br/>"
+            html += "<strong>" + business + "</strong> rated " + rating + " on " + date
+
+    return html
