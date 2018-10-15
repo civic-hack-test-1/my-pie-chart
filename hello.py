56427bbd05efcd9c6b9813685ef6e00c933c7e3f
Andy Mayer <andy@yoomee.com>
2018-10-15 17:28:47 +0100

    Tidy up code

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from xml.dom import minidom
-import pygal                                                       # First import pygal
+import pygal
 
 app = Flask(__name__)
 
@@ -36,35 +36,10 @@ def charts():
     pie_chart.title = 'Restaurants and cafes in Durham'
     pie_chart.add('Rating 1', rating_1)
     pie_chart.add('Rating 2', rating_2)
-    pie_chart.add('Rating 3', rating_1)
-    pie_chart.add('Rating 4', rating_2)
-    pie_chart.add('Rating 5', rating_1)
+    pie_chart.add('Rating 3', rating_3)
+    pie_chart.add('Rating 4', rating_4)
+    pie_chart.add('Rating 5', rating_5)
     pie_chart.add('Awaiting inspection', no_rating)
 
     chart = pie_chart.render_data_uri()
     return render_template( 'chart.html', chart = chart)
-
-    @app.route("/data")
-    def hello():
-        html = "<h1>Restaurants in Durham</h1>"
-
-        xmldoc = minidom.parse("open-data.xml")
-        list = xmldoc.getElementsByTagName("EstablishmentDetail")
-        for n in list:
-            business = n.getElementsByTagName("BusinessName")[0].firstChild.data
-            rating = n.getElementsByTagName("RatingValue")[0].firstChild.data
-            type = n.getElementsByTagName("BusinessType")[0].firstChild.data
-
-            if rating == "AwaitingInspection":
-                rating = "pending"
-
-            if n.getElementsByTagName("RatingDate")[0].firstChild:
-                date = n.getElementsByTagName("RatingDate")[0].firstChild.data
-            else:
-                date = "none"
-
-            if type == "Restaurant/Cafe/Canteen":
-                html += "<br/>"
-                html += "<strong>" + business + "</strong> rated " + rating + " on " + date
-
-        return html
