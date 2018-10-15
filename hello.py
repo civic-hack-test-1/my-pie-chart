1b985a8a2ec367ed2b152a2ad7330e3c8f3c99b2
Andy Mayer <andy@yoomee.com>
2018-10-15 17:23:53 +0100

    Add pie chart

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from xml.dom import minidom
+import pygal                                                       # First import pygal
 
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def hello():
     html = "<h1>Restaurants in Durham</h1>"
 
@@ -27,3 +28,43 @@ def hello():
             html += "<strong>" + business + "</strong> rated " + rating + " on " + date
 
     return html
+
+@app.route('/ratings/')
+def charts():
+    pie_chart = pygal.Pie()
+    xmldoc = minidom.parse("open-data.xml")
+    list = xmldoc.getElementsByTagName("EstablishmentDetail")
+    rating_1 = 0
+    rating_2 = 0
+    rating_3 = 0
+    rating_4 = 0
+    rating_5 = 0
+    no_rating = 0
+
+    for n in list:
+        type = n.getElementsByTagName("BusinessType")[0].firstChild.data
+        if type == "Restaurant/Cafe/Canteen":
+            rating = n.getElementsByTagName("RatingValue")[0].firstChild.data
+            if rating == "1":
+                rating_1 += 1
+            if rating == "2":
+                rating_2 += 1
+            if rating == "3":
+                rating_3 += 1
+            if rating == "4":
+                rating_4 += 1
+            if rating == "5":
+                rating_5 += 1
+            if rating == "AwaitingInspection":
+                no_rating += 1
+
+    pie_chart.title = 'Restaurants and cafes in Durham'
+    pie_chart.add('Rating 1', rating_1)
+    pie_chart.add('Rating 2', rating_2)
+    pie_chart.add('Rating 3', rating_1)
+    pie_chart.add('Rating 4', rating_2)
+    pie_chart.add('Rating 5', rating_1)
+    pie_chart.add('Awaiting inspection', no_rating)
+
+    chart = pie_chart.render_data_uri()
+    return render_template( 'chart.html', chart = chart)
